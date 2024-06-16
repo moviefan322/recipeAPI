8cfbd462849e926dd19430102a15b63726e031b4
moviefan322 <philman202@yahoo.com>
2024-06-16 15:57:15 -0400

    writes a few demo tests

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Caluclator functions
+"""
+
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return value"""
+    return y - x
